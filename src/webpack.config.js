1b6139c93936a981420999b89495ce3f66108a8e
Denisiz <denis.izotov.1994@mail.ru>
2022-03-25 18:00:08 +0300

    вебпак продолжаю настраивать

---
+++ src/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+sourse: path.join(__dirname, 'sourse'),
+build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+
+}
+
+
+
+//1:31 webpack2 - #4 - Pug. отмечу что возможно лучше учиться на 5 вебпаке
